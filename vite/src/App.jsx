fc485ca8a90b53f7e0651ff8585624cdf9969f1d
FullTimeScam <418.gingin@gmail.com>
2024-05-24 14:43:17 +0900

    전송 기능 추가

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -2,12 +2,16 @@ import { useEffect, useState } from "react";
 import MetamaskButton from "./components/MetamaskButton";
 import Erc20Connect from "./components/Erc20Connect";
 import Balance from "./components/balance";
+import { parseEther } from "ethers";
 
 const App = () => {
   const [signer, setSigner] = useState();
   const [contract, setContract] = useState();
   const [name, setName] = useState();
   const [symbol, setSymbol] = useState();
+  const [balance, setBalance] = useState(null);
+  const [transferAddress, setTransferAddress] = useState();
+  const [transferAmount, setTransferAmount] = useState();
 
   const getNameSymbol = async () => {
     try {
@@ -16,6 +20,21 @@ const App = () => {
 
       setName(nameResponse);
       setSymbol(symbolResponse);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const onClickTransfer = async () => {
+    try {
+      if (!transferAddress || !transferAmount) return;
+
+      const response = await contract.transfer(
+        transferAddress,
+        parseEther(transferAmount, "wei")
+      );
+
+      console.log(response);
     } catch (error) {
       console.error(error);
     }
@@ -33,10 +52,39 @@ const App = () => {
       {signer && (
         <div className="mt-16 flex flex-col gap-8 grow max-w-xl w-full">
           <div className="box-style text-center">
-            0x77D2DAC005A952eF61AbC3D5b460bF60c805E790
+            0xa20dED068e956290c459ad3162d78B93a7109be3
           </div>
           <Erc20Connect name={name} signer={signer} setContract={setContract} />
-          {name && <Balance name={name} symbol={symbol} contract={contract} />}
+          {name && (
+            <Balance
+              name={name}
+              symbol={symbol}
+              contract={contract}
+              balance={balance}
+              setBalance={setBalance}
+            />
+          )}
+          {contract && (
+            <div className="flex w-full">
+              <input
+                className="input-style grow"
+                type="text"
+                placeholder="지갑주소"
+                value={transferAddress}
+                onChange={(e) => setTransferAddress(e.target.value)}
+              />
+              <input
+                className="input-style grow"
+                type="text"
+                placeholder="금액"
+                value={transferAmount}
+                onChange={(e) => setTransferAmount(e.target.value)}
+              />
+              <button className="button-style ml-4" onClick={onClickTransfer}>
+                전송
+              </button>
+            </div>
+          )}
         </div>
       )}
     </div>
